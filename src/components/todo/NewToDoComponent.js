7667df8a9fc5281c92edbc4315664492846f720b
Daiber Gonzalez <daibergm@Daibers-MacBook-Pro.local>
2019-10-18 17:48:23 -0500

    feat: Remove checkbox when add todo

--- src/components/todo/NewToDoComponent.js
+++ src/components/todo/NewToDoComponent.js
@@ -1,19 +1,17 @@
 import React, { useState } from 'react';
-import { Body, Icon, CheckBox, ListItem, Input, Button } from 'native-base';
+import { Body, Icon, ListItem, Input, Button } from 'native-base';
 
 const NewToDoComponent = ({ onClick, onCancel }) => {
-  const [completed, setCompleted] = useState(false);
   const [title, setTitle] = useState('');
 
   return (
     <ListItem>
-      <CheckBox checked={completed} onPress={() => setCompleted(!completed)} />
       <Body>
         <Input
           placeholder="Wht needs to be done?"
           onChangeText={value => setTitle(value)}
           onSubmitEditing={() =>
-            onClick({ title, completed, date: new Date().toISOString() })
+            onClick({ title, completed: false, date: new Date().toISOString() })
           }
         />
       </Body>
